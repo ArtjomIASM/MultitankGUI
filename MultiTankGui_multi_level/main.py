5d089aec7db08b30bcdb9b2603b2099a5ca4fd5d
ArtjomIASM <solartty@outlook.com>
2017-05-25 14:35:34 +0300

    Delete main.py

--- MultiTankGui_multi_level/main.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PyQt4.QtGui import QApplication
-from mainwindow import MainWindow
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    wndMain = MainWindow()
-    sys.exit(app.exec_())
